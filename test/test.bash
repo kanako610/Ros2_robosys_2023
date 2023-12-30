2ec2eb150d24e921afceeddf39c824c814ff4374
Kanako Takahashi <jerryt6103@gmail.com>
2023-12-30 15:48:53 +0900

    更新

--- test/test.bash
+++ test/test.bash
@@ -2,14 +2,13 @@
 # SPDX-FileCopyrightText: 2023 Takahashi Kanako 
 # SPDX-License-Identifier: BSD-3-Clause
 
-
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
-colcon build 
-sourseb$dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg/.log
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log
+cat /tmp/mypkg.log 
 grep 'Listen: 10'
