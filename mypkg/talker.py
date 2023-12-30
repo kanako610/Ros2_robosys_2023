682c4319faf1448b8a85c1c2f9ee15d14c7def43
Kanako Takahashi <jerryt6103@gmail.com>
2023-12-30 13:43:23 +0900

    更新

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,30 @@
+# SPDX-FileCopyrightText: © 2023 Kanako Takahashi
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
+
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == "__main__":
+    main()
 
 
 
-def cb(request, response):
-    if request.name == "高橋佳菜子":
-        response.age = 20
-    else:
-        response.age = 255
-
-    return response
-
-
-rclpy.init()
-node = Node("talker")
-srv = node.create_service(Query, "query",cb)
-rclpy.spin(node)
